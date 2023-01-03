cea47d7a03c3cf10a61e35664640e8f73e0778f5
SeyeonJang <seye0n.bliss@gmail.com>
2023-01-03 22:57:26 +0900

    Comment: "activity_main 이랑 MainActivity 파일에 챠코 테스트용 버튼이랑 기능 구현 주석으로 달아놨습니다. 다른 페이지들 구현 완료되면 지우겠습니다" Docs: "PWFoundActivity - IDFoundActivity 먼저 구현하려고 코드 지워뒀습니다"

--- app/src/main/java/com/example/lifolio/SingUp/PWFoundActivity.kt
+++ app/src/main/java/com/example/lifolio/SingUp/PWFoundActivity.kt
@@ -6,13 +6,5 @@ import com.example.lifolio.databinding.ActivityPwfoundBinding
 
 class PWFoundActivity : AppCompatActivity() {
     private lateinit var binding : ActivityPwfoundBinding // binding 변수 선언
-    override fun OnCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityPwfoundBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        binding.idfoundGotologinBtn.setOnClickListener {
-
-        }
-    }
 }
