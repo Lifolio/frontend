b10d3486fcab0f65d12ebfd663ee7d619f689037
SeyeonJang <seye0n.bliss@gmail.com>
2023-01-06 17:08:43 +0900

    Docs: IDFoundActivity 서버 연결 중 Docs: IDRequest 서버 연결 중 Feat: IDResponse 서버에서 받아오는 Data Class 생성 완료

--- app/src/main/java/com/example/lifolio/SingUp/IDRequest.kt
+++ app/src/main/java/com/example/lifolio/SingUp/IDRequest.kt
@@ -1,6 +1,6 @@
 package com.example.lifolio.SingUp
 
-// 챠코 - #
+// 챠코 - #IDFoundActivity와 연결
 
 import retrofit2.http.POST
 
--- app/src/main/java/com/example/lifolio/SingUp/IDResponse.kt
+++ app/src/main/java/com/example/lifolio/SingUp/IDResponse.kt
@@ -1,5 +1,7 @@
 package com.example.lifolio.SingUp
 
+// 챠코 - #IDFoundActivity와 연결
+
 data class IDResponse(
     val isSuccess: Boolean,
     val code: Int,
