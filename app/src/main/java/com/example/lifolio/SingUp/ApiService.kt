82a2960f37c34cc8ee1edbe7f8cdc4f6ec186b95
DongHeon Kwak <rhkrehdgjs@naver.com>
2023-01-06 19:07:09 +0900

    Comment : "ApiService 기능 및 담당자 주석 추가"

--- app/src/main/java/com/example/lifolio/SingUp/ApiService.kt
+++ app/src/main/java/com/example/lifolio/SingUp/ApiService.kt
@@ -7,17 +7,17 @@ import retrofit2.http.Query
 
 interface ApiService {
     @GET ("user/check/userId")
-    fun getCheckUserId(
+    fun getCheckUserId( // 아이디 중복체크 (허니)
         @Query("userId") userId : String
     ): Call<Response>
 
     @GET ("user/check/nickname")
-    fun getCheckUserNickname(
+    fun getCheckUserNickname( // 닉네임 중복체크 (허니)
         @Query("nickName") nickName : String
     ): Call<Response>
 
     @GET ("user/check/sendSMS")
-    fun getSMS(
+    fun getSMS( // sms인증 (허니)
         @Query("to") to : String
     ): Call<Response>
 }
