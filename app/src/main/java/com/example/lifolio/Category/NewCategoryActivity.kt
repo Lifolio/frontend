fce45f7a6683244d8cfdf37fe948f63620b5a051
sylee <sylee6529@gmail.com>
2023-01-14 16:47:29 +0900

    Feat: 카테고리 추가 activity 추가

---
+++ app/src/main/java/com/example/lifolio/Category/NewCategoryActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lifolio.Category
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lifolio.R
+
+class NewCategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_category)
+    }
+}
