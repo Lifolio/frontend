de7cd49d01c14363f634563cf493a89d92da5ca7
sylee <sylee6529@gmail.com>
2023-01-19 19:23:10 +0900

    Feat: BigCategoryFragment 카테고리 추가 구현 완료 (동적 칩 생성)

--- app/src/main/java/com/example/lifolio/Category/BigCategoryFragment.kt
+++ app/src/main/java/com/example/lifolio/Category/BigCategoryFragment.kt
@@ -4,39 +4,46 @@ import android.content.Context
 import android.content.res.ColorStateList
 import android.os.Bundle
 import android.util.TypedValue
+import android.view.KeyEvent
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.example.lifolio.PortfolioFragment
 import com.example.lifolio.R
 import com.example.lifolio.databinding.FragmentBigCategoryBinding
+import com.example.lifolio.databinding.ItemCategoryChipBinding
+import com.google.android.flexbox.FlexboxLayout
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipGroup
+import kotlin.math.roundToInt
 
 class BigCategoryFragment : Fragment() {
     private lateinit var binding : FragmentBigCategoryBinding
     private lateinit var addCategoryActivity: AddCategoryActivity
-    private lateinit var chipGroup: ChipGroup
+    private lateinit var chipGroup : ChipGroup
 
     companion object {
         const val TAG : String = "BIG_CATEGORY_FRAGMENT"
 
-        fun newInstance() : PortfolioFragment {
-            return PortfolioFragment()
+        fun newInstance() : BigCategoryFragment {
+            return BigCategoryFragment()
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
     }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
 
-        // 2. Context를 액티비티로 형변환해서 할당
-        addCategoryActivity = context as AddCategoryActivity
+        addCategoryActivity = context as AddCategoryActivity // Context를 부모 액티비티로 형변환해서 할당, context 필요시 사용
     }
 
     override fun onCreateView(
@@ -45,50 +52,47 @@ class BigCategoryFragment : Fragment() {
     ): View? {
         binding = FragmentBigCategoryBinding.inflate(layoutInflater)
 
-         // Dummy Chip 4개 출력
-//        chipGroup = binding.bigCategoryChipgroupCg
-//        for (i in 0..3) {
-//            val chip = ItemCategoryChipBinding.inflate(layoutInflater).root as Chip
-//
-//            chip.text = "항목$i"
-////            chip.chipStrokeColor = ContextCompat.getColorStateList(this, R.color.chip_selector_stroke_color)  // 안됨
-////            chip.chipStrokeColor = myList
-//
-//            chip.setEnsureMinTouchTargetSize(false)
-//            chip.chipBackgroundColor = ColorStateList.valueOf(
-//                ContextCompat.getColor(
-//                    addCategoryActivity,
-//                    R.color.chip_selector_background_color
-//                )
-//            )
-//            chip.chipStrokeColor = ColorStateList.valueOf(
-//                ContextCompat.getColor(
-//                    addCategoryActivity,
-//                    R.color.chip_selector_primary_color
-//                )
-//            )
-//            chip.setTextColor(
-//                ColorStateList.valueOf(
-//                    ContextCompat.getColor(
-//                        addCategoryActivity,
-//                        R.color.chip_selector_primary_color
-//                    )
-//                )
-//            )
-//            chip.chipStartPadding = 50F
-//            chip.chipEndPadding = 40F
-//
-////            chip.isCheckable = true // 체크 표시 사용 여부
-////            chip.closeIcon = getDrawable(R.drawable.ic_close) // chip close 아이콘 이미지 지정
-////            chip.isCloseIconVisible = true // close icon 표시 여부
-//
-//            chip.chipStrokeWidth = 5.5f
-//            chip.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 12f)
-//
-//
-//
-//            chipGroup!!.addView(chip) // chip group 에 해당 chip 추가
-//        }
+        binding.bigCategoryChipEt.setOnKeyListener { v, keyCode, event ->
+            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                val et = v as EditText
+                val name = et.text.toString()
+
+                binding.bigCategoryFlexboxlt.addChip(name)
+
+                et.text = null
+            }
+            return@setOnKeyListener false
+        }
+
         return binding.root
     }
+
+}
+
+private fun FlexboxLayout.addChip(text: String) {
+    val chip = LayoutInflater.from(context).inflate(R.layout.item_category_chip, null) as Chip
+    chip.text = text
+
+    val layoutParams = ViewGroup.MarginLayoutParams(ViewGroup.MarginLayoutParams.WRAP_CONTENT, ViewGroup.MarginLayoutParams.WRAP_CONTENT)
+    layoutParams.rightMargin = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 4F, resources.displayMetrics).roundToInt()
+
+    chip.setOnCloseIconClickListener {
+        removeView(chip as View)
+    }
+
+    addView(chip, childCount - 1, layoutParams)
+}
+
+private fun FlexboxLayout.getAllChips(): List<Chip> {
+    return (0 until childCount).mapNotNull { index ->
+        getChildAt(index) as? Chip
+    }
+}
+
+private fun FlexboxLayout.clearChips() {
+    val chipViews = (0 until childCount).mapNotNull { index ->
+        val view = getChildAt(index)
+        if (view is Chip) view else null
+    }
+    chipViews.forEach { removeView(it) }
 }
