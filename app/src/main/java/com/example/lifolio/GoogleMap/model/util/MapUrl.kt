d8c5e8a516be1aa4cdeec511e72d4f544171be98
sylee <sy>
2023-01-30 02:53:39 +0900

    Feat: Tmap 위한 Object Class 작성

---
+++ app/src/main/java/com/example/lifolio/GoogleMap/model/util/MapUrl.kt
@@ -0,0 +1,9 @@
+package com.example.lifolio.GoogleMap.model.util
+
+object MapUrl {
+    const val TMAP_URL = "https://apis.openapi.sk.com"
+
+    const val GET_TMAP_LOCATION = "/tmap/pois"
+
+    const val GET_TMAP_REVERSE_GEO_CODE = "/tmap/geo/reversegeocoding"
+}
