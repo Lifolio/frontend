1fa20e2e2da4bda38a353825ec6828255671756e
sylee <sy>
2023-02-14 21:53:53 +0900

    Refactor: service 파일 이름 수정

--- app/src/main/java/com/example/lifolio/Category/CategoryService.kt
+++ app/src/main/java/com/example/lifolio/Category/CategoryService.kt
@@ -1,15 +1,13 @@
 package com.example.lifolio.Category
 
-import android.provider.ContactsContract.Data
 import com.example.lifolio.Category.model.PostCategoryReq
 import com.example.lifolio.util.model.BaseRes
 import retrofit2.Call
 import retrofit2.http.Body
-import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Path
 
-interface PostCategoryService {
+interface CategoryService {
     @POST("category/list/{userId}")
     fun addBigCategory(@Body postCategoryReq: PostCategoryReq, @Path("userId") userId: Int): Call<BaseRes>
 }
